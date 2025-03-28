c96b577bd4c2db98f4a97c6aad0dadfd49e5a3a3
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-28 21:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ let counter = 0;
 
 document.getElementById('incrementBtn').addEventListener('click', function() {
     alert("Current Counter Value: " + counter);
-    counter++;
-    document.getElementById('counter').textContent = counter;
+    document.getElementById('counter').textContent = ++counter;
 });
